58d2b53b1bbdc1b94ea722766534436a80227a64
Mario Geiger <geiger.mario@gmail.com>
2012-04-21 13:50:11 +0200

    bouttons undo redo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
+#include <QToolBar>
+#include <QAction>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QToolBar *toolbar = new QToolBar(this);
+    QPixmap pix(":/images/undo.png");
+    undoAction = toolbar->addAction(QIcon(pix),
+                                    "undo", this, SLOT(actionUndo()));
+    undoAction->setEnabled(false);
+    redoAction = toolbar->addAction(QIcon(pix.transformed(QTransform::fromScale(-1, 1))),
+                                    "redo", this, SLOT(actionRedo()));
+    redoAction->setEnabled(false);
+    addToolBar(toolbar);
+
     _pacientView = new QGraphicsView(this);
     _pacientScene = new PacientScene(this);
     _pacientView->setScene(_pacientScene);
+    _pacientView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    _pacientView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     setCentralWidget(_pacientView);
 }
@@ -13,4 +27,12 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     
+}
+
+void MainWindow::actionUndo()
+{
+}
+
+void MainWindow::actionRedo()
+{
 }
