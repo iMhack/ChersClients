29632d0561e8be60ede071f72ffccfb33c200127
Quentin Talon <quentin.talon@gmail.com>
2012-04-23 22:51:08 +0200

    Essai d'enregistrement de .ini (att pour mac avec ../../../mesoptions.ini)

--- bac_a_sable.cpp
+++ bac_a_sable.cpp
@@ -14,13 +14,38 @@ Bac_a_sable::~Bac_a_sable()
 }
 
 #include <QApplication>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     Bac_a_sable w;
+    QSettings settings("../../../mesoptions.ini", QSettings::IniFormat);
+    settings.setValue("Identite/NOM","");
+    settings.setValue("Identite/Prénom","");
+    settings.setValue("Identite/Date de naissance","");
+    settings.setValue("Identite/Adresse","");
+    settings.setValue("Identite/Envoyé par","");
+    settings.setValue("Identite/Profession","");
+    settings.setValue("Identite/Tel privé","");
+    settings.setValue("Identite/Portable","");
+    settings.setValue("Identite/Assurance","");
+    settings.setValue("Identite/_","");
+    settings.setValue("Anamnèse/Motif de consultation","");
+    settings.setValue("Anamnèse/Siège","");
+    settings.setValue("Anamnèse/Irradiation","");
+    settings.setValue("Anamnèse/Qualité","");
+    settings.setValue("Anamnèse/Intensité","");
+    settings.setValue("Anamnèse/ChronologieDébut","");
+    settings.setValue("Anamnèse/ChronologieDurée","");
+    settings.setValue("Anamnèse/ChronologieFréquence","");
+    settings.setValue("Anamnèse/Allérgies","");
+    settings.setValue("Anamnèse/Tabac","");
+    settings.setValue("Anamnèse/Alcool","");
+
     w.show();
+
 
     return app.exec();
 }
--- bac_a_sable.pro
+++ bac_a_sable.pro
@@ -3,4 +3,5 @@ SOURCES += bac_a_sable.cpp
 HEADERS += bac_a_sable.h
 
 FORMS += bac_a_sable.ui
+TEMPLATE = app
 
